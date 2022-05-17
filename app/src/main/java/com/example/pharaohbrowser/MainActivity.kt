866f2581caa2f90f05a7a234beec1c71b7103dd1
RamesusXD <j1gamer4life@gmail.com>
2022-05-16 17:25:06 -0700

    updates: added links to popular social sites. started on functionality on MainActivity.kt file for the webview and URL search bar. additional code clean up

--- app/src/main/java/com/example/pharaohbrowser/MainActivity.kt
+++ app/src/main/java/com/example/pharaohbrowser/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.pharaohbrowser
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var searchView: SearchView? = null
+    var webview: WebView? = null
+    var progressBar: ProgressBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchView = findViewById(R.id.search_view)
+        webview = findViewById(R.id.webView)
+        progressBar = findViewById(R.id.progress_bar)
+
+        webview!!.settings.javaScriptEnabled = true
+        webview!!.webViewClient = WebViewClient() //displays web sites
     }
+
+    fun imageClicked(view: View) {
+        var id=view.id
+        var ourId: String? =""
+
+        if (isInternetConnected()) {
+
+            ourId = view.resources.getResourceEntryName(id)
+            webview?.loadUrl("https://www." + ourId + ".com")
+            webview!!.visibility = View.VISIBLE
+            mainScreen!!.visibility = View.GONE
+        } else {
+            Toast.makeText(this,
+                    "ummm there is no internet connection. turn on wifi or make sure there is a data connection.",
+                    Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+
+
+        fun iconClicked(view: View) {}
+
+        fun isInternetConnected(): Boolean {
+            val connectivityManager: ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkinfo = connectivityManager.activeNetworkInfo
+            return networkinfo != null && networkinfo.isConnected
+        }
+
+    }
+
+    private fun isInternetConnected(): Boolean {
+        val connectivityManager: ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkinfo = connectivityManager.activeNetworkInfo
+        return networkinfo != null && networkinfo.isConnected
+
+    }
+
 }
